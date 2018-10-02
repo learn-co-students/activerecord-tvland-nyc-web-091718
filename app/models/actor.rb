a3ba9bc823ec9d0a67d4435dda46e2cb4a89d015
Trinacria <xcstolfax@gmail.comgit config --global user.name Christopher Stolfagit config --global user.email xcstolfax@gmail.com>
2018-10-02 12:09:31 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,15 @@
+require 'pry'
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  #Write a method in the Actor class, #full_name, that returns the first and last name of an actor.
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    self.characters.all.map {|character|  "#{character.name} - #{character.show.name}"}
+  end
+
 end
