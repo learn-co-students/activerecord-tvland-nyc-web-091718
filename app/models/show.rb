dc2c810a4a8d386f7a09d03db497d1e524ed5103
github username <github email address>
2018-10-20 19:34:52 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
 end
+
+# An actor has many characters and has many shows through characters.
+# A character belongs to an actor and belongs to a show.
+# A show has many characters and has many actors through characters.
